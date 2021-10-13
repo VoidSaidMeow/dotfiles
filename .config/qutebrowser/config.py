82627ff50f922f7cfd6ffcbc2310b485916e3b72
VoidSaidMeow <pandelibezolli@gmail.com>
2021-10-13 11:32:59 +0300

    Enabled add blocker

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,4 @@
 config.load_autoconfig()
-config.set("colors.webpage.darkmode.enabled", True)
-config.set("fonts.default_size", "5pt" )
+config.set( "colors.webpage.darkmode.enabled", True )
+config.set( "fonts.default_size", "5pt" )
+config.set( "content.blocking.method", "both" )
